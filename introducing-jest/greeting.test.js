2a1e09958568668e527f425f254c3f0dfdd33994
moshfiqrony <moshfiqrony@gmail.com>
2020-06-08 10:46:27 +0600

    MR | :white_check_mark: final commit

--- introducing-jest/greeting.test.js
+++ introducing-jest/greeting.test.js
@@ -10,30 +10,33 @@ describe('greeting()', () => {
 
 describe('divided by 3 and 5 or not', () => {
     test('test case true', () => {
-        expect(findMe(15)).toBe('Rony')
+        expect(findMe(15)).toBe('Moshfiq Rony');
     })
 
     test('test case true 2', () => {
-        expect(findMe(30)).toBe('Rony')
+        expect(findMe(30)).toBe('Moshfiq Rony');
     })
 
     test('test case true 3', () => {
-        expect(findMe(45)).toBe('Rony')
+        expect(findMe(45)).toBe('Moshfiq Rony');
     })
 
     test('test case false 1', () => {
-        expect(findMe(16)).not.toBe('Rony')
+        expect(findMe(16)).not.toBe('Moshfiq Rony');
     })
 
     test('test case false 2', () => {
-        expect(findMe(70)).not.toBe('Rony')
+        expect(findMe(70)).not.toBe('Moshfiq Rony');
     })
 
     test('test case false 3', () => {
-        expect(findMe(50)).not.toBe('Rony')
+        expect(findMe(50)).not.toBe('Moshfiq Rony');
     })
 
-    test('test case false 4', () => {
-        expect(findMe(100)).toBe(100)
+    test('test case true divided by 5', () => {
+        expect(findMe(100)).toBe('Rony');
+    })
+    test('test case true divided by 3', () => {
+        expect(findMe(9)).toBe('Moshfiq');
     })
 })
--- introducing-jest/index.js
+++ introducing-jest/index.js
@@ -1,6 +1,10 @@
 module.exports = num => {
     if(num % 15 === 0){
+        return('Moshfiq Rony');
+    }else if(num % 3 === 0){
+        return 'Moshfiq'
+    }else if (num % 5 === 0){
         return('Rony')
     }
-    return num
+    return num;
 }
